7ea287c55ad5644435df9108dc789d81cf1cbfc5
fio.ucs <fio.ucs@kakaoenterprise.com>
2023-03-28 16:56:40 +0900

    Initial commit

---
+++ src/main/java/com/publisher/fragranceia_back/FragranceiaBackApplication.java
@@ -0,0 +1,13 @@
+package com.publisher.fragranceia_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragranceiaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragranceiaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publisher/fragranceia_back/FragranceiaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publisher.fragranceia_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragranceiaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
